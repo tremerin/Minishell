68b0f41748e6d5d219a64617c44ca9c34851c90e
tremerin <102490188+tremerin@users.noreply.github.com>
2023-03-07 15:20:16 +0100

    Add files via upload

--- PipexPlus/pipex.c
+++ PipexPlus/pipex.c
@@ -1,9 +1,9 @@
 #include "pipex.h"
 
-char **envp_split(char **envp)
+char	**envp_split(char **envp)
 {
-	int     i;
-	char    **paths;
+	int		i;
+	char	**paths;
 
 	i = 0;
 	while (envp[i] != NULL)
@@ -24,9 +24,9 @@ char **envp_split(char **envp)
 	return (NULL);
 }
 
-void free_split(char **split)
+void	free_split(char **split)
 {
-	int     i;
+	int		i;
 
 	i = 0;
 	while (split[i] != NULL)
@@ -39,7 +39,7 @@ void free_split(char **split)
 	free(split);
 }
 
-char *get_cmd(char **paths, char *cmd)
+char	*get_cmd(char **paths, char *cmd)
 {
 	char	*command;
 
@@ -54,12 +54,11 @@ char *get_cmd(char **paths, char *cmd)
 	return (NULL);
 }
 
-void    exe_command(t_pipex pipex, char *cmd, char **envp)
+void	exe_command(t_pipex *pipex, char *cmd, char **envp)
 {
-	//printf("exe\n");
-	pipex.command_args = ft_split(cmd, ' ');
-	pipex.command = get_cmd(pipex.paths, pipex.command_args[0]);
-	execve(pipex.command, pipex.command_args, envp);
+	pipex->command_args = ft_split(cmd, ' ');
+	pipex->command = get_cmd(pipex->paths, pipex->command_args[0]);
+	execve(pipex->command, pipex->command_args, envp);
 }
 
 void	close_fd(t_pipex *pipex)
@@ -67,10 +66,11 @@ void	close_fd(t_pipex *pipex)
 	int		i;
 
 	i = 0;
-	while (0 < pipex->n_cmd -1)
+	while (i < pipex->n_cmd -1)
 	{
 		close(pipex->fd[i][0]);
 		close(pipex->fd[i][1]);
+		i++;
 	}
 }
 
@@ -78,16 +78,20 @@ void	dup_fd(t_pipex *pipex, int children)
 {
 	if (children == 0)
 	{
+		if (pipex->in_fd != -2)
+			dup2(pipex->in_fd, STDIN_FILENO);
 		dup2(pipex->fd[0][1], STDOUT_FILENO);
 	}
 	else if (children == pipex->n_cmd -1)
 	{
-		dup2(pipex->fd[children][0], STDIN_FILENO);
+		if (pipex->out_fd != -2)
+			dup2(pipex->out_fd, STDOUT_FILENO);
+		dup2(pipex->fd[children -1][0], STDIN_FILENO);
 	}
 	else
 	{
 		dup2(pipex->fd[children][1], STDOUT_FILENO);
-		dup2(pipex->fd[children -1][0], STDIN_FILENO);
+		dup2(pipex->fd[children - 1][0], STDIN_FILENO);
 	}
 }
 
@@ -109,7 +113,7 @@ void	exe_pipex(t_pipex *pipex, char **argv, char **envp)
 		{
 			dup_fd(pipex, i);
 			close_fd(pipex);
-			exe_command(*pipex, argv[i], envp);
+			exe_command(pipex, argv[i + 1], envp);
 		}
 		i++;
 	}
@@ -117,26 +121,26 @@ void	exe_pipex(t_pipex *pipex, char **argv, char **envp)
 	waitpid(-1, NULL, 0);
 }
 
-int main(int argc, char **argv, char**envp)
+int main(int argc, char **argv, char **envp)
 {
-	t_pipex     pipex;
+	t_pipex		pipex;
 
+	pipex.outfile = ft_strdup("./file.txt");
+	if (pipex.outfile != NULL)
+		pipex.out_fd = open(pipex.outfile, O_TRUNC | O_CREAT | O_RDWR, 0664);
 	pipex.n_cmd = argc -1;
 	pipex.paths = envp_split(envp);
 	exe_pipex(&pipex, argv, envp);
+	if (pipex.outfile != NULL)
+		close(pipex.out_fd);
 	free_split(pipex.paths);
-	
 	return (0);
 }
 
-
-
-
-
 /* main original funcionando */
-// int main(int argc, char **argv, char**envp)
+// int	main(int argc, char **argv, char **envp)
 // {
-// 	t_pipex     pipex;
+// 	t_pipex		pipex;
 // 	pid_t		pid1;
 // 	pid_t		pid2;
 // 	pid_t		pid3;
@@ -145,60 +149,47 @@ int main(int argc, char **argv, char**envp)
 // 	if (argc > 1)
 // 	{
 // 		pipex.paths = envp_split(envp);
-
 // 		pipe(fd[0]);
 // 		pipe(fd[1]);
-
-		
 // 		pid1 = fork();
-		
 // 		if (pid1 == 0)
 // 		{
-// 			//printf("pid 1, pid: %d\n", getpid());
+// 			dup2(fd[0][1], STDOUT_FILENO);
 // 			close(fd[0][0]);
+// 			close(fd[0][1]);
 // 			close(fd[1][0]);
 // 			close(fd[1][1]);
-// 			dup2(fd[0][1], STDOUT_FILENO);
-// 			close(fd[0][1]);
-// 			exe_command(pipex, argv[1], envp);
-// 		}	
-		
+// 			exe_command(&pipex, argv[1], envp);
+// 		}
 // 		pid2 = fork();
-
 // 		if (pid2 == 0)
 // 		{
-// 			//printf("pid 2 pid: %d\n", getpid());
-			
-// 			close(fd[0][1]);
-// 			close(fd[1][0]);
 // 			dup2(fd[1][1], STDOUT_FILENO);
 // 			dup2(fd[0][0], STDIN_FILENO);
 // 			close(fd[0][0]);
+// 			close(fd[0][1]);
+// 			close(fd[1][0]);
 // 			close(fd[1][1]);
-// 			exe_command(pipex, argv[2], envp);
+// 			exe_command(&pipex, argv[2], envp);
 // 		}
-
 // 		pid3 = fork();
-
 // 		if (pid3 == 0)
 // 		{
-// 			//printf("pid 3 pid: %d\n", getpid());
+// 			dup2(fd[1][0], STDIN_FILENO);
 // 			close(fd[0][0]);
 // 			close(fd[0][1]);
 // 			close(fd[1][1]);
-// 			dup2(fd[1][0], STDIN_FILENO);
-// 			close(fd[1][0]);	
-// 			exe_command(pipex, argv[3], envp);
-// 		}	
-
+// 			close(fd[1][0]);
+// 			exe_command(&pipex, argv[3], envp);
+// 		}
 // 		close(fd[0][0]);
 // 		close(fd[0][1]);
 // 		close(fd[1][0]);
 // 		close(fd[1][1]);
-// 		waitpid(pid1, NULL, 0);
-// 		waitpid(pid2, NULL, 0);
-// 		waitpid(pid3, NULL, 0);	
-		
+// 		//waitpid(pid1, NULL, 0);
+// 		//waitpid(pid2, NULL, 0);
+// 		//waitpid(pid3, NULL, 0);
+// 		waitpid(-1, NULL, 0);
 // 		free_split(pipex.paths);
 // 	}
 // 	return (0);
--- PipexPlus/pipex.h
+++ PipexPlus/pipex.h
@@ -25,12 +25,16 @@
 
 typedef struct s_pipex
 {
-    char    **paths;
-    char    *command;
-    char    **command_args;
-    int     fd[10][2];
-    pid_t   pid[10];
-    int     n_cmd;
-}   t_pipex;
+	char	**paths;
+	char	*command;
+	char	**command_args;
+	int		fd[10][2];
+	pid_t	pid[10];
+	int		n_cmd;
+	char	*infile;
+	char	*outfile;
+	int		in_fd;
+	int		out_fd;
+}	t_pipex;
 
-# endif
+#endif
